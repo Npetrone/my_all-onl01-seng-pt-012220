c0eeebcc90c83bf2e612d8969d7ceaba6dd208ef
Nicholas Petrone <nickpetrone92@gmail.com>
2020-01-31 01:07:30 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,9 +4,9 @@ def my_all?(collection)
   i=0
   block_return_values = []
   while i < collection.length
- 
   block_return_values << yield(collection[i])
-  i += 1binding.pry 
+  i += 1
+  binding.pry 
 end
 
 if block_return_values.include?(false)
